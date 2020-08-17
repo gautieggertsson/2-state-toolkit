0709f72d141dc573cae073cee6cba2e162c81bb2
Luca Riva <lucariva.work@gmail.com>
2020-08-17 23:53:14 +0200

    Fix Examples/Common

--- Examples/Common/matrices.m
+++ Examples/Common/matrices.m
@@ -36,23 +36,15 @@ AAA(eq.pi_lag,vars.pi_lag) = 1;
 BBB(eq.pi_lag,vars.pi)     = 1;
 
 % Truncated Taylor rule (past inflation and output, smoothed)
-AAA(eq.rule,vars.pi)       = 0;
-AAA(eq.rule,vars.x)        = 0;
 BBB(eq.rule,vars.i)        = -1;
 BBB(eq.rule,vars.rstar)    = 1-param.phi_i;
 BBB(eq.rule,vars.pi_lag)   = (1-param.phi_i)*param.phi_pi;
 BBB(eq.rule,vars.i_lag)    = param.phi_i;
 BBB(eq.rule,vars.x_lag)    = (1-param.phi_i)*param.phi_x; 
-BBB(eq.rule,vars.pi)       = 0;
-BBB(eq.rule,vars.x)        = 0;
 
 % Implied Taylor rule  
-AAA(eq.i_imp,vars.pi)      = 0;
-AAA(eq.i_imp,vars.x)       = 0;
 BBB(eq.i_imp,vars.i_imp)   = -1;
 BBB(eq.i_imp,vars.rstar)   = 1-param.phi_i;
 BBB(eq.i_imp,vars.pi_lag)  = (1-param.phi_i)*param.phi_pi;
 BBB(eq.i_imp,vars.i_lag)   = param.phi_i;
 BBB(eq.i_imp,vars.x_lag)   = (1-param.phi_i)*param.phi_x; 
-BBB(eq.i_imp,vars.pi)      = 0;
-BBB(eq.i_imp,vars.x)       = 0;
